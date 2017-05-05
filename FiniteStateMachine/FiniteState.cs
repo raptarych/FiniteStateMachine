e12d2703042c1dd35ddda774304ba9cfeef7ff23
raptarych <raptarych@gmail.com>
2017-05-05 09:39:31 +0500

    Теперь алгоритм минимизации вроде работает хорошо, помогло:  http://mathhelpplanet.com/static.php?p=minimizatsiya-konechnykh-avtomatov

--- FiniteStateMachine/FiniteState.cs
+++ FiniteStateMachine/FiniteState.cs
@@ -39,13 +39,15 @@ namespace FiniteStateMachine
                 .Value.Select(val => val.Key)
                 .Where(val => val != "Output")
                 .ToList();
+            //  1) Группировка
+            //  1.1) 0-эквивалентность
             var grouping = PossibleStates.GroupBy(elem => elem.Value["Output"]).ToList();
             Console.WriteLine($"Current grouping: {string.Join(",", grouping.Select(group => $"{{{string.Join(",", group.Select(elem => elem.Key))}}}"))}");
 
             var currentSymbol = "";
 
-            //1) Группировка
 
+            //  1.2) 1-эквивалентность
             foreach (var symbol in allowedSymbols)
             {
                 currentSymbol = symbol;
@@ -58,6 +60,22 @@ namespace FiniteStateMachine
                 }).ToList();
                 Console.WriteLine($"Current grouping: {string.Join(",", grouping.Select(group => $"{{{string.Join(",", group.Select(elem => elem.Key))}}}"))}");
             }
+
+            //  1.3) 2-эквивалентность
+            grouping = PossibleStates.GroupBy(arg =>
+            {
+                var currentGroup = grouping.FirstOrDefault(gr => gr.Any(grElem => grElem.Key == arg.Key));
+                var currentGroupName = currentGroup?.Key;
+                var returnString = $"{currentGroupName}_";
+                foreach (var symbol in allowedSymbols)
+                {
+                        
+                    var group2 = grouping.FirstOrDefault(gr => gr.Any(grElem => grElem.Key == arg.Value[symbol]))?.Key;
+                    returnString += $"{symbol}{group2}";
+                }
+                return returnString;
+            })
+            .ToList();
 
             if (grouping.All(group => group.Count() <= 1))
             {
